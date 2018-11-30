1bf548d08bf35a38eb1c66079ded8985d3d9c59f
Dariusz Pelka <dariusz.pelka@almservices.pl>
2018-11-30 13:49:30 +0100

    Dodanie opcji do dodania nowej firmy

---
+++ SampleWebApp/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNewCompany()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewCompany(Company model)
+        {
+            return View(model);
+        }
+    }
+}
